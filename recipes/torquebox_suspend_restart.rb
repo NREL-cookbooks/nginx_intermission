99dcf1aea44588af05d728abeba07fd22bda328f
Nick Muerdter <nick.muerdter@nrel.gov>
2014-07-17 18:47:14 -0600

    Switch torquebox restart script from ruby script to bash script.
    The ruby script was doing funky things with the system calls when jruby
    was the default system ruby. Plus, ruby was way overkill for this script
    now that it's just checking files for success (rather than parsing JSON
    results).

--- recipes/torquebox_suspend_restart.rb
+++ recipes/torquebox_suspend_restart.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "rbenv::system"
+include_recipe "curl"
 
 template "/usr/local/bin/torquebox_suspend_restart" do
   source "torquebox_suspend_restart.erb"
